9bb0671fa86f7d060641eaa3930b655be988a7e7
Artem Safin <artemsafin.webdev@gmail.com>
2022-06-17 23:18:29 +0300

    Фикс

--- src/js/selectService.js
+++ src/js/selectService.js
@@ -1,15 +1,23 @@
 export default function selectService() {
     const modalInput = document.querySelector('.js-selected-service');
     const selector = '.service__card-link-wrapper[href^="#"]';
-    if (!modalInput) return;
-    document.addEventListener('click', event => {
-        if (event.target.matches(selector) || event.target.closest(selector)) {
-            const link = event.target.matches(selector) ? event.target : event.target.closest(selector);
-            const title = link.querySelector('.service-card__title');
+    if (modalInput) {
+        document.addEventListener('click', event => {
+            if (event.target.matches(selector) || event.target.closest(selector)) {
+                const wrapper = event.target.matches(selector) ? event.target : event.target.closest(selector);
+                const link = wrapper.closest('.service-card');
+                if (link) {
+                    const title = link.querySelector('.service-card__title');
 
-            if (title) {
-                modalInput.value = title.textContent;
+                    if (title) {
+                        modalInput.value = title.textContent;
+
+                        console.log('Value inserted');
+                    }
+                } else {
+                    console.log('No link');
+                }
             }
-        }
-    })
+        });
+    }
 }
